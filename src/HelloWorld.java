4d735ead247b1d80500230507989362f6353f404
Joshua Randall <jranda10@emich.edu>
2023-10-02 18:48:09 -0400

    Added a few more words to second println()

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Alternate print statement.");
+		System.out.println("Alternate print statement with a few more words.");
 	}
 }
